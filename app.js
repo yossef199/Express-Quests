2a8d817b833595cbeb07ead285759197b62f2dee
romaing <rocambille@gmail.com>
2022-01-11 12:28:21 +0100

    fixed to match quest updates

--- app.js
+++ app.js
@@ -1,44 +1,48 @@
-const connection = require('./db-config');
-const express = require('express');
+const connection = require("./db-config");
+const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ?? 3000;
 
 connection.connect((err) => {
   if (err) {
-    console.error('error connecting: ' + err.stack);
+    console.error("error connecting: " + err.stack);
   } else {
-    console.log('connected as id ' + connection.threadId);
+    console.log("connected as id " + connection.threadId);
   }
 });
 
 app.use(express.json());
 
-app.get('/api/movies', (req, res) => {
-  connection.query('SELECT * FROM movies', (err, result) => {
+app.get("/api/movies", (req, res) => {
+  connection.query("SELECT * FROM movies", (err, result) => {
     if (err) {
-      res.status(500).send('Error retrieving data from database');
+      res.status(500).send("Error retrieving data from database");
     } else {
       res.json(result);
     }
   });
 });
 
-app.post('/api/movies', (req, res) => {
+app.post("/api/movies", (req, res) => {
   const { title, director, year, color, duration } = req.body;
   connection.query(
-    'INSERT INTO movies (title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)',
+    "INSERT INTO movies (title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
     [title, director, year, color, duration],
     (err, result) => {
       if (err) {
-        res.status(500).send('Error saving the movie');
+        res.status(500).send("Error saving the movie");
       } else {
-        res.status(201).send('Movie successfully saved');
+        res.status(200).send("Movie successfully saved");
       }
     }
   );
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
 });
