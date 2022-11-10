8a14bdada552e1cc186da45a8ef621af97a4c274
yossef199 <kassabiyossef@gmail.Com>
2022-11-10 16:50:44 +0100

    commit 6

--- movieHandlers.js
+++ movieHandlers.js
@@ -28,8 +28,23 @@ const movies = [
 ];
 
 const getMovies = (req, res) => {
+  let sql = "select * from movies";
+  const sqlValues = [];
+
+  if (req.query.color != null) {
+    sql += "where color = ?";
+    sqlValues.push(req.query.color);
+    if (req.query.max_duration != null) {
+      sql += "and duration <= ?";
+      sqlValues.push(req.query.max_duration);
+    }
+  } else if (req.query.max_duration != null) {
+    sql += "where duration <= ?";
+    sqlValues.push(req.query.max_duration);
+  }
+
   database
-    .query("select * from movies")
+    .query(sql, sqlValues)
     .then(([movies]) => {
       res.json(movies);
     })
@@ -40,14 +55,26 @@ const getMovies = (req, res) => {
 };
 
 const getUsers = (req, res) => {
+  let sql = "select * from users";
+  const sqlValues = [];
+
+  if (req.query.language != null) {
+    sql += " where language = ?";
+    sqlValues.push(req.query.language);
+
+    if (req.query.city != null) {
+      sql += " and city git remote= ?";
+      sqlValues.push(req.query.city);
+    }
+  } else if (req.query.city != null) {
+    sql += " where city = ?";
+    sqlValues.push(req.query.city);
+  }
+
   database
-    .query("select * from users")
+    .query(sql, sqlValues)
     .then(([users]) => {
-      if (users.length) {
-        res.json(users);
-      } else {
-        res.status(404).send("Not Found");
-      }
+      res.json(users);
     })
     .catch((err) => {
       console.error(err);
